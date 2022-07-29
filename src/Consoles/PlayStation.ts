c38fc301b9f0fa7b20db2bbe547735411f303e34
Vinicius Alves Beló <vinialvesbelo@outlook.com>
2022-07-29 10:15:40 -0300

    Second Commit

--- src/Consoles/PlayStation.ts
+++ src/Consoles/PlayStation.ts
@@ -1,6 +1,12 @@
 import IConsole from "./IConsole";
 
 export default class PlayStation implements IConsole{
+
+    constructor(){
+        this.configuraGame()
+        console.log('PlayStation Conectado.')
+    }
+
     configuraGame(): void {
         this.authToken()
         console.log('PlayStation: Configurando Jogo.')
--- src/Consoles/XBox.ts
+++ src/Consoles/XBox.ts
@@ -1,6 +1,12 @@
 import IConsole from "./IConsole";
 
 export default class XBox implements IConsole{
+
+    constructor(){
+        this.configuraGame()
+        console.log('XBox Conectado.')
+    }
+
     configuraGame(): void {
         this.authToken()
         console.log('XBox: Configurando Jogo.')
